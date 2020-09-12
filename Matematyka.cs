493fc16e280b8cb8641e62d385fdaaf5df61de2c
Programista-z-Polski <70840444+Programista-z-Polski@users.noreply.github.com>
2020-09-12 15:30:58 +0200

    Add files via upload

---
+++ Matematyka.cs
@@ -0,0 +1,19 @@
+﻿namespace Mat
+{
+    using System;
+    class Mat
+    {
+        decimal PierwKw(int b)
+        {
+            return Convert.ToDecimal(Math.Sqrt(b));
+        }
+        decimal Pierw(int b, int e)
+        {
+            return Convert.ToDecimal(Math.Pow(b, (1 / e)));
+        }
+        ulong Potega(int a, int e)
+        {
+            return Convert.ToUInt64(Math.Pow(a, e));
+        }
+    }
+}
