225e41767cc8ed7d843bb45188217ad0de5fe23e
Programista-Cpp <70840444+Programista-Cpp@users.noreply.github.com>
2020-11-17 09:02:11 +0100

    Update Mat.cpp

--- Sposzczone-jezyki-programowania/Spolszczone-jezyki-programowania/VCPP/Mat.cpp
+++ Sposzczone-jezyki-programowania/Spolszczone-jezyki-programowania/VCPP/Mat.cpp
@@ -2,44 +2,44 @@
 #include <math.h>
 using namespace std;
 
-namespace mat
+namespace Mat
 {
     static const double Pi = 3.14159265359;
     static double Odwrotnosc(double x)
     {
-        funkcje::Zwroc(1 / x);
+        Funkcje::Zwroc(1 / x);
     }
     static double Przeciwna(double x)
     {
-        funkcje::Zwroc(-x);
+        Funkcje::Zwroc(-x);
     }
 
     static double Pierw2(double arg)
     {
-        funkcje::Zwroc(sqrt(arg));
+        Funkcje::Zwroc(sqrt(arg));
     }
     static double Pierw(double arg, int grad)
     {
-        funkcje::Zwroc(pow(arg, (1 / grad)));
+        Funkcje::Zwroc(pow(arg, (1 / grad)));
     }
     static long double Potega(long double arg, unsigned int exp)
     {
-        funkcje::Zwroc(pow(arg, exp));
+        Funkcje::Zwroc(pow(arg, exp));
     }
 
     namespace srednia
     {
         static double SredniaArytm(double a, double b, double c, double d, double e, double f, double g)
         {
-            funkcje::Zwroc((a + b + c + d + e + f + g) / 7);
+            Funkcje::Zwroc((a + b + c + d + e + f + g) / 7);
         }
         static double SredniaGeometryczna(double a, double b, double c, double d, double e, double f, double g)
         {
-            funkcje::Zwroc(mat::Pierw(a * b * c * d * e * f * g, 7));
+            Funkcje::Zwroc(Mat::Pierw(a * b * c * d * e * f * g, 7));
         }
         static double SredniaHarmoniczna(double a, double b, double c, double d, double e, double f, double g)
         {
-            funkcje::Zwroc(7 / ((1 / a) + (1 / b) + (1 / c) + (1 / d) + (1 / e) + (1 / f) + (1 / g)));
+            Funkcje::Zwroc(7 / ((1 / a) + (1 / b) + (1 / c) + (1 / d) + (1 / e) + (1 / f) + (1 / g)));
         }
     };
 
@@ -47,60 +47,60 @@ namespace mat
     {
         static double PoleKola(double r) //Pole Kola
         {
-            funkcje::Zwroc(Pi * mat::Potega(r, 2));
+            Funkcje::Zwroc(Pi * Mat::Potega(r, 2));
         }
         static double ObwodKola(double r) //Obwod Kola
         {
-            funkcje::Zwroc(2 * Pi * r);
+            Funkcje::Zwroc(2 * Pi * r);
         }
         static int ObwodTrojkata(int a, int b, int c)
         {
-            funkcje::Zwroc(a + b + c);
+            Funkcje::Zwroc(a + b + c);
         }
         static int PoleTrapezu(int a, int b, int h) //Pole Trapezu
         {
-            funkcje::Zwroc(((a + b) * h) / 2);
+            Funkcje::Zwroc(((a + b) * h) / 2);
         }
         static int PoleRownolegloboku(int a, int h) //Pole Rownolegloboku
         {
-            funkcje::Zwroc(a * h);
+            Funkcje::Zwroc(a * h);
         }
         static int PoleProstokata(int a, int b) //Pole Prostokata
         {
-            funkcje::Zwroc(a * b);
+            Funkcje::Zwroc(a * b);
         }
         static int PoleRombu(int e, int f) //Pole Rombu
         {
-            funkcje::Zwroc((e * f) / 2);
+            Funkcje::Zwroc((e * f) / 2);
         }
         static int PoleKwadratu(int a) //Pole Kwadratu
         {
-            funkcje::Zwroc(mat::Potega(a, 2));
+            Funkcje::Zwroc(Mat::Potega(a, 2));
         }
         static int PoleTrojkata(int a, int h) //Pole Trojkata
         {
-            funkcje::Zwroc((a * h) / 2);
+            Funkcje::Zwroc((a * h) / 2);
         }
     };
     namespace specjalneFunkcje
     {
         static int abs(int x) //Wartosc Bezwzgledna
         {
-            funkcje::Zwroc(abs(x));
+            Funkcje::Zwroc(abs(x));
         }
         static int Sgn(int x) //Funkcja Signum
         {
-            if (x > 0) funkcje::Zwroc(1);
-            else if (x == 0) funkcje::Zwroc(0);
-            else funkcje::Zwroc(-1);
+            if (x > 0) Funkcje::Zwroc(1);
+            else if (x == 0) Funkcje::Zwroc(0);
+            else Funkcje::Zwroc(-1);
         }
         static double Hiperbola(int x) //Hiperbola
         {
-            funkcje::Zwroc(Odwrotnosc(x));
+            Funkcje::Zwroc(Odwrotnosc(x));
         }
         static int Parabola(int x) //Parabola
         {
-            funkcje::Zwroc(mat::Potega(x, 2));
+            Funkcje::Zwroc(Mat::Potega(x, 2));
         }
     };
 };
