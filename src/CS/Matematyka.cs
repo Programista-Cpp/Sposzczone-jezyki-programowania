1a7127731ca5ae7cb20acfa54ae37302d4fbc926
ankiedos <70840444+ankiedos@users.noreply.github.com>
2020-12-15 20:32:32 +0100

    Added folders src, img, docs, .vscode. Updated Cpp.hpp. Added LICENSE and documentation.

---
+++ src/CS/Matematyka.cs
@@ -0,0 +1,19 @@
+﻿namespace Mat
+{
+    using System;
+    public class Mat
+    {
+        static decimal PierwKw(int b)
+        {
+            return Convert.ToDecimal(Math.Sqrt(b));
+        }
+        static decimal Pierw(int b, int e)
+        {
+            return Convert.ToDecimal(Math.Pow(b, (1 / e)));
+        }
+        static ulong Potega(int a, int e)
+        {
+            return Convert.ToUInt64(Math.Pow(a, e));
+        }
+    }
+}
---
+++ src/PLCPP/pl_cpp_ext/Funkcje.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+namespace Funkcje
+{
+	template <typename T>
+	static T Zwroc(T arg)
+	{
+		return arg;
+	}
+};
